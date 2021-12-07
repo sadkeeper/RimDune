7c5986da7808b65bab6aa76cfff8a42471556ecb
sadkeeper <timentnobis@gmail.com>
2021-12-07 22:06:03 +1100

    + minable spice
    some stuff trying to figure out the voice too but not working

---
+++ Source/RimDune/RimDune/DamageWorker_Voice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace RimDune
+{
+	public class DamageWorker_Voice : DamageWorker
+	{
+
+	}
+}
